0e930b979505d3b40ff548e62ddd516a0ce00668
JhouXu <2324837581@qq.com>
2021-02-24 17:22:36 +0800

    修复效果展示上的问题。

--- js/musicPlayPause.js
+++ js/musicPlayPause.js
@@ -1,13 +1,26 @@
+musicPlayPause('.n-bgmusic-img img', '.n-bgmusic-music');
+
 /**
- * 控制背景音乐动画的播放暂停的切换，以及控制音乐播放暂停
+ * 主要功能：控制背景音乐动画的播放暂停的切换，以及控制音乐播放暂停。
+ * 实现思路：首先设置初始状态为暂停，然后判断是否为微信内置浏览器，如果是就播放，如果不是就显示未播放按钮，引导用户点击图标从而实现播放功能。
  * @param {string} img 图片节点img
  * @param {string} music 音乐audio元素节点
  */
 function musicPlayPause(img, music) {
   let imgDom = document.querySelector(img);
   let musicDom = document.querySelector(music);
-  let isOpen = true;
+  let isOpen = false;
 
+  // 监听微信内置浏览器是否加载完成（只有微信端口才可以调用）
+  document.addEventListener(
+    'WeixinJSBridgeReady',
+    function () {
+      musicPlay();
+    },
+    false
+  );
+
+  // 绑定音乐图片的点击事件
   imgDom.addEventListener('click', function () {
     if (isOpen) {
       musicPause();
@@ -21,15 +34,6 @@ function musicPlayPause(img, music) {
     musicDom.play();
     imgDom.src = 'https://s3.ax1x.com/2021/02/24/yOfu3F.png';
     isOpen = true;
-
-    // 监听微信内置浏览器是否加载完成
-    document.addEventListener(
-      'WeixinJSBridgeReady',
-      function () {
-        musicDom.play();
-      },
-      false
-    );
   }
 
   // 暂停
